5de70db43f486eb510e1a7f2ec8672af5ba556c6
vaishnavikpatil <vaishnavipatil108@gmail.com>
2025-04-09 21:53:22 +0530

    commit

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+// api/index.js
+
 const express = require('express')
+const serverless = require('serverless-http')
 const dotenv = require('dotenv')
-const connectDB = require('./config/db')
+const connectDB = require('../config/db')
 
 dotenv.config()
 connectDB()
@@ -8,13 +11,11 @@ connectDB()
 const app = express()
 app.use(express.json())
 
-const authRoutes = require('./routes/authRoutes')
-const productRoutes = require('./routes/productRoutes')
+const authRoutes = require('../routes/authRoutes')
+const productRoutes = require('../routes/productRoutes')
 
 app.use('/api/auth', authRoutes)
 app.use('/api/products', productRoutes)
 
-const PORT = process.env.PORT || 3000
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+// Export the app wrapped in serverless-http
+module.exports = serverless(app)
